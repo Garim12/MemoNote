58e6ab0e979d863ada4a0e8fdf23e4aef71e4074
MSM <qw12345611@naver.com>
2023-06-05 18:50:44 +0900

    Ver.1
    삭제 기능 구현

--- src/org/example/MemoList.java
+++ src/org/example/MemoList.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -27,14 +29,13 @@ public class MemoList {
 
     // 메모 정보 출력
     public void printMemoList() {
-        int i = 1; // 몇번째 메모인지 출력하기 위해 변수 선언
+        Collections.sort(memoList, new idComparator());
         for(Memo memo : memoList){
             if(memo!=null) {
                 System.out.println("\n=====================================");
-                System.out.printf(" [[ %-2d번째 메모 ]] ",i);
+                System.out.printf(" [[ %-2d번째 메모 ]] ", memo.getId());
                 memo.printMemo();
                 System.out.println();
-                i++;
             }else {// if() of the end
                 System.out.println("저장된 메모가 없습니다.");
             } // if ~ else() of the end
@@ -80,6 +81,18 @@ public class MemoList {
     private void reindexMemo() {
         for (int i = 0; i < memoList.size(); i++) {
             memoList.get(i).setId(i + 1);
+        }
+    }
+
+    class idComparator implements Comparator<Memo> {
+        @Override
+        public int compare(Memo o1, Memo o2) {
+            if (o1.getId() < o2.getId()) {
+                return 1;
+            } else if (o1.getId() > o2.getId()) {
+                return -1;
+            }
+            return 0;
         }
     }
 
