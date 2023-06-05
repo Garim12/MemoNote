c1bbdabcb4d50afd1985ac30d765717f9a8fadd1
jeon youngmin <wjsdudals56@naver.com>
2023-06-05 17:28:16 +0900

    최신 등록 순으로 정렬

--- src/org/example/MemoList.java
+++ src/org/example/MemoList.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MemoList {
@@ -26,19 +28,28 @@ public class MemoList {
 
     // 메모 정보 출력
     public void printMemoList() {
-        int i = 1; // 몇번째 메모인지 출력하기 위해 변수 선언
+        Collections.sort(memoList, new idComparator());
         for(Memo memo : memoList){
             if(memo!=null) {
                 System.out.println("\n=====================================");
-                System.out.printf(" [[ %-2d번째 메모 ]] ",i);
+                System.out.printf(" [[ %-2d번째 메모 ]] ", memo.getId());
                 memo.printMemo();
                 System.out.println();
-                i++;
             }else {// if() of the end
                 System.out.println("저장된 메모가 없습니다.");
             } // if ~ else() of the end
         }// for() of the end
     }
+}
 
-    
+class idComparator implements Comparator<Memo> {
+    @Override
+    public int compare(Memo o1, Memo o2) {
+        if (o1.getId() < o2.getId()) {
+            return 1;
+        } else if (o1.getId() > o2.getId()) {
+            return -1;
+        }
+        return 0;
+    }
 }
