795811a7266deb56db02e0f00d352dff26ae8fe2
MSM <qw12345611@naver.com>
2023-06-05 19:57:25 +0900

    Ver.2
    삭제

--- src/org/example/MemoList.java
+++ src/org/example/MemoList.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -29,13 +27,14 @@ public class MemoList {
 
     // 메모 정보 출력
     public void printMemoList() {
-        Collections.sort(memoList, new idComparator());
+        int i = 1; // 몇번째 메모인지 출력하기 위해 변수 선언
         for(Memo memo : memoList){
             if(memo!=null) {
                 System.out.println("\n=====================================");
-                System.out.printf(" [[ %-2d번째 메모 ]] ", memo.getId());
+                System.out.printf(" [[ %-2d번째 메모 ]] ",i);
                 memo.printMemo();
                 System.out.println();
+                i++;
             }else {// if() of the end
                 System.out.println("저장된 메모가 없습니다.");
             } // if ~ else() of the end
@@ -54,6 +53,7 @@ public class MemoList {
 
 
     }
+
 
     // 해당 글 삭제하는 메서드
     public void deleteMemo(int delNum) {
@@ -81,18 +81,6 @@ public class MemoList {
     private void reindexMemo() {
         for (int i = 0; i < memoList.size(); i++) {
             memoList.get(i).setId(i + 1);
-        }
-    }
-
-    class idComparator implements Comparator<Memo> {
-        @Override
-        public int compare(Memo o1, Memo o2) {
-            if (o1.getId() < o2.getId()) {
-                return 1;
-            } else if (o1.getId() > o2.getId()) {
-                return -1;
-            }
-            return 0;
         }
     }
 }
